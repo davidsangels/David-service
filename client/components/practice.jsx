a9d351c31a11894c971f5d9e0b80bc5ffb9333a5
David Brooks <davidbrooks@Davids-MacBook-Pro-2.local>
2019-07-11 09:28:21 -0700

    react

---
+++ client/components/practice.jsx
@@ -0,0 +1,28 @@
+
+import React from 'react'
+
+class Ap extends React.Component {
+
+  constructor (props){
+    super(props)
+
+    }
+
+    render() {
+      return(
+      <div className='container' >
+        <p className='item'>1</p>
+        <p className='item'>2</p>
+        <p className='item'>3</p>
+        <p className='item'>4</p>
+        <p className='item'>5</p>
+        <p className='item'>6</p>
+        <p className='item'>7</p>
+        <p className='item'>8</p>
+        <p className='item'>9</p>
+        <p className='item'>10</p>
+      </div>
+      )
+    }
+}
+export default Ap
--- client/index.jsx
+++ client/index.jsx
@@ -2,5 +2,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx'
+import Ap from './components/practice.jsx'
 
-ReactDOM.render(<App/ >,document.getElementById('app'))
+ReactDOM.render(<App />,document.getElementById('app'))
