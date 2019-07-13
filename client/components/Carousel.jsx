6223a8c76deab7306913f135f3324071bfa0df21
David Brooks <davidbrooks@Davids-MacBook-Pro-2.local>
2019-07-13 16:44:29 -0700

    Add image size

--- client/components/Carousel.jsx
+++ client/components/Carousel.jsx
@@ -14,7 +14,8 @@ class Carousel extends React.Component {
     <div className='carousel'>
       <div className='carousel-container' >
         <div className='image-wrapper' style={{transform: `translateX(-${this.props.offset}px)`}}  >
-          {this.props.images.map((img, index) => (<div>
+          {this.props.images.map( (img, index) => (
+          <div >
             <img className='image' onClick={()=> this.props.changeView(img.imgUrl)}
             src={img.imgUrl}
             key={index} />
