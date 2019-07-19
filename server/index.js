a1a99720e129e432deee6c5e4c9dd88ac31cd631
David Brooks <davidbrooks@Davids-MacBook-Pro-2.local>
2019-07-19 16:39:31 -0700

    refactor server for id

--- server/index.js
+++ server/index.js
@@ -7,6 +7,27 @@ const db = require('../database/seed.js');
 server.use(bodyParser());
 server.use(express.static('public'));
 
+
+server.use('/:id', express.static('public'));
+
+
+server.get('/data/:id', (req, res) => {
+  
+  let id = req.params.id
+  let groupId = Number(id.slice(1))
+
+    console.log('ee')
+    connection.connection.query('select * from images where id = ?', groupId, (err, result) => {
+      if (err) {
+        throw err;
+      } else {
+        res.status(200).send(result);
+      }
+    });
+});
+
+
+
 server.get('/data', (req, res) => {
   db.query('select * from images where id = 1', (err, result) => {
     if (err) {
@@ -16,4 +37,6 @@ server.get('/data', (req, res) => {
     }
   });
 });
+
+
 server.listen(port, () => console.log(`server listening on port : ${port}`));
